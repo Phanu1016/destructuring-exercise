6a5c20ca0022e45c225a96ed7aa6ab85bebda1c5
Phanu <admin@phanuphanu.com>
2022-07-09 11:07:51 -0700

    Array Destructuring 1

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// Object Destructuring 3
-function getUserData({firstName, favoriteColor="green"}){
-    return `Your name is ${firstName} and you like ${favoriteColor}`;
-  }
-  
-getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // Your name is Alejandro and you like purple
-getUserData({firstName: "Melissa"}) // Your name is Melissa and you like green
-getUserData({}) // Your name is undefined and you like green
+// Array Destructuring 1
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
