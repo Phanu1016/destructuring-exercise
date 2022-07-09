59e7dc75c20fbf2d96a3d34d8664cd695dbffa5b
Phanu <admin@phanuphanu.com>
2022-07-09 11:03:12 -0700

    Object Destructuring 1

---
+++ app.js
@@ -0,0 +1,6 @@
+// Object Destructuring 1
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
