aa614ba77e7a44abb60019cf35ebb10e7ec0e54f
Phanu <admin@phanuphanu.com>
2022-07-09 11:19:43 -0700

    raceResults()

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-// ES5 Array Swap
-// var arr = [1, 2];
-// var temp = arr[0];
-// arr[0] = arr[1];
-// arr[1] = temp;
-
-// ES2015 One-Line Array Swap with Destructuring
-[arr[0], arr[1]] = [arr[1], arr[0]]
+// Write a function called raceResults which accepts a single array argument. It should return an object with the keys first, second, third, and rest.
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
