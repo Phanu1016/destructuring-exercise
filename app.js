15229074fa7e5c6b5af15c07f1a622583728a9f0
Phanu <admin@phanuphanu.com>
2022-07-09 11:08:54 -0700

    Array Destructuring 2

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// Array Destructuring 1
-let [first, second, third] = ["Maya", "Marisa", "Chi"];
-
-console.log(first); // Maya
-console.log(second); // Marisa
-console.log(third); // Chi
+// Array Destructuring 2
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+console.log(raindrops); // Raindrops on roses
+console.log(whiskers); // whiskers on kittens
+console.log(aFewOfMyFavoriteThings); // ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
