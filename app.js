6361704f0c5484be9bab3fedb591066c2b65556e
Phanu <admin@phanuphanu.com>
2022-07-09 11:15:19 -0700

    ES2015 One-Line Array Swap with Destructuring

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-// ES5 Assigning Variables to Object Properties
-// var obj = {
-//     numbers: {
-//       a: 1,
-//       b: 2
-//     }
-//   };
-  
-//   var a = obj.numbers.a;
-//   var b = obj.numbers.b;
+// ES5 Array Swap
+// var arr = [1, 2];
+// var temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
 
-// ES2015 Object Destructuring
-const obj = {
-    numbers: {
-      a: 1,
-      b: 2
-    }
-  };
-
-const {a, b} = obj.numbers
+// ES2015 One-Line Array Swap with Destructuring
+[arr[0], arr[1]] = [arr[1], arr[0]]
